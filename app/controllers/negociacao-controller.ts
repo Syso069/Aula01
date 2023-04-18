2a1ffdd53d71ff3f861bfa25ce75f785a2982d6a
Syso <sisomd69@gmail.com>
2023-04-18 02:07:37 -0300

    otimizando codigo

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -12,11 +12,24 @@ export class NegociacaoController {
     }
 
     adicionar (){
+        this.criaNegociacao;
+        this.limpaFormulario;
+    }
+
+    criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
         const negociacao = new Negociacao(date, quantidade, valor);
-        console.log(negociacao);
+        return new Negociacao(date, quantidade, valor);
     }
-}
+
+    limpaFormulario(): void {
+        this.inputData.value='';
+        this.inputQuantidade.value='';
+        this.inputValor.value='';
+        this.inputData.focus();
+    }
+
+ }
