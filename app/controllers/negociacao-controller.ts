da23aa644b312449c471e58f3daf4a6b12a64170
Syso <sisomd69@gmail.com>
2023-04-18 20:26:15 -0300

    avancando na modelagem do dominio

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new  Negociacoes;
 
     constructor (){
         this.inputData = document.querySelector('#data');
@@ -11,9 +13,11 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
 
-    adicionar (){
-        this.criaNegociacao;
-        this.limpaFormulario;
+    adicionar (): void{
+        const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
+        this.limpaFormulario();
     }
 
     criaNegociacao(): Negociacao {
